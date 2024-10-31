b41c713d252df5b665d863be5fa550f13654096d
m1rroxdev <alekc0086@gmail.com>
2024-10-31 22:40:08 +0200

    create project

--- main.js
+++
@@ -1,9 +0,0 @@
-const deskSize = {width: 8, height: 8};
-
-const checkersNumber = 24
-
-const clearDesk = createDesk(deskSize);
-
-const desk = initGame(checkersNumber, clearDesk)
-
-console.log(desk)
